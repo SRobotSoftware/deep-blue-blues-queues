d7a3dbe0baf23aff000916de4a43ec2e2c838d60
Jason Nguyen <jason123nguyen@gmail.com>
2016-07-16 10:48:03 -0600

    jason final push

--- app/queue-component/queue-controller.js
+++ app/queue-component/queue-controller.js
@@ -3,46 +3,39 @@
     var qs = new QuestionService();
 
     var elem = $("#questions");
+    var qCount = $("#count");
 
     function update() {
-        qs.getQuestions(function(questions){
+        qs.getQuestions(function (questions) {
             elem.empty()
+            qCount.text(Object.keys(questions).length)
             for (var id in questions) {
                 var currentQuestion = questions[id];
                 elem.append(`
-                <br>
-                <div class="row text-center">
+                <div class="text-center">
                     <div class="container">
-                    <div class="col-lg-6">
-                    <div class="card" id="${currentQuestion.id}">
-                        <div class="panel panel-primary">
-                            <div class="panel-heading">
-                                <h2>${currentQuestion.author}</h2>
-                                <h4>${currentQuestion.email}</h4>
-                            </div>
-                                <div class="panel-body">
-
-
-                                
-                                <p>${currentQuestion.topic}</p>
-                                <p>${currentQuestion.question}</p>
-                                <p>${currentQuestion.posted}</p>
-                                <p>${currentQuestion.status}</p>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-success answer-button">Answer</button>
-                                    <button type="button" class="btn btn-info resolved-button">Resolve</button>
-                                    <button type="button" class="btn btn-danger delete-button">Dismiss</button>
-                                </div>
-
-
-
+                        <div class="col-lg-offset-3 col-lg-6">
+                            <div class="card" id="${currentQuestion.id}">
+                                <div class="panel panel-primary">
+                                    <div class="panel-heading">
+                                        <h2>${currentQuestion.author}</h2>
+                                        <h4>${currentQuestion.email}</h4>
+                                    </div>
+                                    <div class="panel-body">
+                                        <p>${currentQuestion.topic}</p>
+                                        <p>${currentQuestion.question}</p>
+                                        <p>${currentQuestion.posted}</p>
+                                        <p>${currentQuestion.status}</p>
+                                        <div class="btn-group">
+                                            <button type="button" class="btn btn-success answer-button">Answer</button>
+                                            <button type="button" class="btn btn-info resolved-button">Resolve</button>
+                                            <button type="button" class="btn btn-danger delete-button">Dismiss</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        
-                    </div>
-                    </div>
-                </div>`)
+                    </div>`)
             }
         });
 
@@ -54,9 +47,9 @@
         update()
     })
 
-update()
+    update()
 
-}())
+} ())
 
 
 
