c69b7fb1dde9a36db9d6f905f5e49a71f7d7f0ea
Jason Nguyen <jason123nguyen@gmail.com>
2016-07-16 14:31:42 -0600

    Jason Final Push

--- app/queue-component/queue-controller.js
+++ app/queue-component/queue-controller.js
@@ -11,31 +11,27 @@
             qCount.text(Object.keys(questions).length)
             for (var id in questions) {
                 var currentQuestion = questions[id];
-                elem.append(`
+                elem.prepend(`
                 <div class="text-center">
-                    <div class="container">
-                        <div class="col-lg-offset-3 col-lg-6">
-                            <div class="card" id="${currentQuestion.id}">
-                                <div class="panel panel-primary">
-                                    <div class="panel-heading">
-                                        <h2>${currentQuestion.author}</h2>
-                                        <h4>${currentQuestion.email}</h4>
-                                    </div>
-                                    <div class="panel-body">
-                                        <p>${currentQuestion.topic}</p>
-                                        <p>${currentQuestion.question}</p>
-                                        <p>${moment(currentQuestion.posted)}</p>
-                                        <p>${currentQuestion.status}</p>
-                                        <div class="btn-group">
-                                            <button type="button" class="btn btn-success answer-button">Answer</button>
-                                            <button type="button" class="btn btn-info resolved-button">Resolve</button>
-                                            <button type="button" class="btn btn-danger delete-button">Dismiss</button>
-                                        </div>
-                                    </div>
+                    <div class="col-lg-offset-3 col-lg-6">
+                        <div class="card" id="${currentQuestion.id}">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <h2>${currentQuestion.author}</h2>
+                                    <h4>${currentQuestion.email}</h4>
+                                </div>
+                                <div class="panel-body">
+                                    <p class="capital">${currentQuestion.topic}</p>
+                                    <p>${currentQuestion.question}</p>
+                                    <p class="date">${moment(currentQuestion.posted)}</p>
+                                    <p class="capital">${currentQuestion.status}</p>
+                                    <div class="col-lg-4 col-lg-offset-4">
+                                    <button type="button" class="btn btn-danger delete-button btn-block">Dismiss</button>
                                 </div>
                             </div>
                         </div>
-                    </div>`)
+                    </div>
+                </div>`)
             }
         });
 
