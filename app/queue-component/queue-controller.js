5456641ba3de267a3db63edad445e6dc67d56d90
Jason Nguyen <jason123nguyen@gmail.com>
2016-07-16 10:17:30 -0600

    jason last push

--- app/queue-component/queue-controller.js
+++ app/queue-component/queue-controller.js
@@ -10,17 +10,37 @@
             for (var id in questions) {
                 var currentQuestion = questions[id];
                 elem.append(`
-                <div class="card" id="${currentQuestion.id}">
-                    <p>${currentQuestion.author}</p>
-                    <p>${currentQuestion.email}</p>
-                    <p>${currentQuestion.topic}</p>
-                    <p>${currentQuestion.question}</p>
-                    <p>${currentQuestion.posted}</p>
-                    <p>${currentQuestion.status}</p>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-success answer-button">Answer</button>
-                        <button type="button" class="btn btn-info resolved-button">Resolve</button>
-                        <button type="button" class="btn btn-danger delete-button">Dismiss</button>
+                <br>
+                <div class="row text-center">
+                    <div class="container">
+                    <div class="col-lg-6">
+                    <div class="card" id="${currentQuestion.id}">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <h2>${currentQuestion.author}</h2>
+                                <h4>${currentQuestion.email}</h4>
+                            </div>
+                                <div class="panel-body">
+
+
+                                
+                                <p>${currentQuestion.topic}</p>
+                                <p>${currentQuestion.question}</p>
+                                <p>${currentQuestion.posted}</p>
+                                <p>${currentQuestion.status}</p>
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-success answer-button">Answer</button>
+                                    <button type="button" class="btn btn-info resolved-button">Resolve</button>
+                                    <button type="button" class="btn btn-danger delete-button">Dismiss</button>
+                                </div>
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
                     </div>
                 </div>`)
             }
